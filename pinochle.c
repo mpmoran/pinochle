5a9706369f6acc4264e38581410993487519f606
Michael P. Moran <30704827+mpmoran@users.noreply.github.com>
2022-05-21 01:37:29 -0500

    add sources; add some "counter" card logic

--- pinochle.c
+++ pinochle.c
@@ -1,6 +1,13 @@
 /*
  * notes and code for learning pinochle
+ *
+ * by michael p. moran
+ * 
+ * SOURCES
+ * https://bicyclecards.com/how-to-play/pinochle-2/
+ * https://gamerules.com/rules/pinochle-card-game/
  */ 
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -16,11 +23,19 @@ enum rank {
     jack,
     nine
 };
-// "counter" cards are ALWAYS worth points.
+/* "counter" cards are ALWAYS worth points. */
 const enum rank COUNTERS[] = {
     ace,
     ten,
     king
+};
+/* sometimes there's such a thing as "noncounter" cards.
+ * these are worthless (i.e., no points).
+ */
+const enum rank NONCOUNTERS[] = {
+    queen,
+    jack,
+    nine
 };
 
 enum suit {
@@ -41,6 +56,17 @@ void card_init(struct card *card, enum rank rank, enum suit suit)
     card->rank = rank;
 }
 
+int card_is_counter(struct card *card)
+{
+    enum rank rank = card->rank;
+    for (unsigned long i = 0; i < sizeof(COUNTERS); i++) {
+        if (rank == COUNTERS[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 int main(int argc, char **argv) {
     printf("Hello.\n");
     if(argc != 1) {
@@ -53,6 +79,12 @@ int main(int argc, char **argv) {
     printf("I think we got a card.\n");
     card_init(first_card, ace, clubs);
     printf("The first card has rank %d and suit %d.\n", first_card->rank, first_card->suit);
+    printf("Is the first card a \"counter\" card?\n");
+    if (card_is_counter(first_card) == 1) {
+        printf("Yes.\n");
+    } else {
+        printf("No.\n");
+    }
     printf("Destroying the first card.\n");
     free(first_card);
 
