14711be2f273b470ace563c244601c0159e14024
Michael P. Moran <30704827+mpmoran@users.noreply.github.com>
2022-05-21 00:29:43 -0500

    add build file and main c file

---
+++ pinochle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define PROJECT_NAME "pinochle"
+
+enum suits {
+    clubs,
+    diamonds,
+    hearts,
+    spades
+};
+
+int main(int argc, char **argv) {
+    if(argc != 1) {
+        printf("%s takes no arguments.\n", argv[0]);
+        return 1;
+    }
+    printf("This is project %s.\n", PROJECT_NAME);
+    return 0;
+}
