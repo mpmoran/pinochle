28ed8d8b15181d8b36bb63678d96ac5e333e9c68
Michael P. Moran <30704827+mpmoran@users.noreply.github.com>
2022-05-21 01:18:54 -0500

    switch rank and suit enums; add some code stuff

--- pinochle.c
+++ pinochle.c
@@ -2,17 +2,11 @@
  * notes and code for learning pinochle
  */ 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define PROJECT_NAME "pinochle"
 
 const int PACK_CARD_COUNT = 48;
-
-enum suit {
-    clubs,
-    diamonds,
-    hearts,
-    spades
-};
 
 enum rank {
     ace,
@@ -22,10 +16,18 @@ enum rank {
     jack,
     nine
 };
+// "counter" cards are ALWAYS worth points.
 const enum rank COUNTERS[] = {
     ace,
     ten,
     king
+};
+
+enum suit {
+    clubs,
+    diamonds,
+    hearts,
+    spades
 };
 
 struct card {
@@ -33,17 +35,27 @@ struct card {
     enum rank rank;
 };
 
-void card_init(struct card *card, enum suit suit, enum rank rank)
+void card_init(struct card *card, enum rank rank, enum suit suit)
 {
     card->suit = suit;
     card->rank = rank;
 }
 
 int main(int argc, char **argv) {
+    printf("Hello.\n");
     if(argc != 1) {
         printf("%s takes no arguments.\n", argv[0]);
         return 1;
     }
     printf("This is project %s.\n", PROJECT_NAME);
+
+    struct card *first_card = malloc(sizeof(struct card));
+    printf("I think we got a card.\n");
+    card_init(first_card, ace, clubs);
+    printf("The first card has rank %d and suit %d.\n", first_card->rank, first_card->suit);
+    printf("Destroying the first card.\n");
+    free(first_card);
+
+    printf("Goodbye.\n");
     return 0;
 }
