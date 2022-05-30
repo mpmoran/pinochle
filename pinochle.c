23a43c311304e8c33072406463431b23a12ef9ad
Michael P. Moran <30704827+mpmoran@users.noreply.github.com>
2022-05-29 23:38:13 -0500

    fix tests; add another one

--- pinochle.c
+++ pinochle.c
@@ -811,13 +811,16 @@ player_tests()
     GString* n11 = g_string_new("paulie walnuts");
     struct player *p11 = player_new(n11->str, 0);
     assert(p11->id == 2);
-    assert(g_string_equal(p11->name, n11) == 0);
+    assert(g_string_equal(p11->name, n11) == 1);
     assert(player_is_dealer(p11) == 0);
     assert(card_list_count(p11->hand) == 0);
     g_string_free(n11, FALSE);
     player_free(p11);
 
     /* test is_dealer() */
+    struct player *p21 = player_new("ralphie cifaretto", 1);
+    assert(player_is_dealer(p21) == 1);
+    player_free(p21);
 
     printf("[+] Finished tests for player.\n");
 }
